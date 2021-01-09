42a73774f7b1a9e0e67f48deccf1842ff8eb9992
Takeshi <yamazakitakeshi02@gmail.com>
2021-01-09 21:56:26 +0900

    基本形完成

--- SerialPortThreadExperiment/Form1.Designer.cs
+++ SerialPortThreadExperiment/Form1.Designer.cs
@@ -30,12 +30,104 @@ namespace SerialPortThreadExperiment
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.txtCommNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCommOpen = new System.Windows.Forms.Button();
+            this.btnCommClose = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnSendCR = new System.Windows.Forms.Button();
+            this.lblReceivedText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCommNumber
+            // 
+            this.txtCommNumber.Location = new System.Drawing.Point(48, 6);
+            this.txtCommNumber.Name = "txtCommNumber";
+            this.txtCommNumber.Size = new System.Drawing.Size(28, 19);
+            this.txtCommNumber.TabIndex = 0;
+            this.txtCommNumber.Text = "3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM";
+            // 
+            // btnCommOpen
+            // 
+            this.btnCommOpen.Location = new System.Drawing.Point(82, 4);
+            this.btnCommOpen.Name = "btnCommOpen";
+            this.btnCommOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnCommOpen.TabIndex = 2;
+            this.btnCommOpen.Text = "OPEN";
+            this.btnCommOpen.UseVisualStyleBackColor = true;
+            this.btnCommOpen.Click += new System.EventHandler(this.btnCommOpen_Click);
+            // 
+            // btnCommClose
+            // 
+            this.btnCommClose.Enabled = false;
+            this.btnCommClose.Location = new System.Drawing.Point(163, 4);
+            this.btnCommClose.Name = "btnCommClose";
+            this.btnCommClose.Size = new System.Drawing.Size(75, 23);
+            this.btnCommClose.TabIndex = 3;
+            this.btnCommClose.Text = "CLOSE";
+            this.btnCommClose.UseVisualStyleBackColor = true;
+            this.btnCommClose.Click += new System.EventHandler(this.btnCommClose_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // btnSendCR
+            // 
+            this.btnSendCR.Location = new System.Drawing.Point(14, 47);
+            this.btnSendCR.Name = "btnSendCR";
+            this.btnSendCR.Size = new System.Drawing.Size(224, 23);
+            this.btnSendCR.TabIndex = 4;
+            this.btnSendCR.Text = "CR送信";
+            this.btnSendCR.UseVisualStyleBackColor = true;
+            this.btnSendCR.Click += new System.EventHandler(this.btnSendCR_Click);
+            // 
+            // lblReceivedText
+            // 
+            this.lblReceivedText.BackColor = System.Drawing.Color.White;
+            this.lblReceivedText.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblReceivedText.Location = new System.Drawing.Point(255, 9);
+            this.lblReceivedText.Name = "lblReceivedText";
+            this.lblReceivedText.Size = new System.Drawing.Size(396, 392);
+            this.lblReceivedText.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(672, 420);
+            this.Controls.Add(this.lblReceivedText);
+            this.Controls.Add(this.btnSendCR);
+            this.Controls.Add(this.btnCommClose);
+            this.Controls.Add(this.btnCommOpen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCommNumber);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtCommNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCommOpen;
+        private System.Windows.Forms.Button btnCommClose;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnSendCR;
+        private System.Windows.Forms.Label lblReceivedText;
     }
 }
 
--- SerialPortThreadExperiment/Form1.cs
+++ SerialPortThreadExperiment/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace SerialPortThreadExperiment
 {
@@ -15,6 +16,96 @@ namespace SerialPortThreadExperiment
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnCommOpen_Click(object sender, EventArgs e)
+        {
+            Comm1Open();
+
+            // シリアルポートが開く前用のコントロールを無効化
+            btnCommOpen.Enabled = false;
+
+            // シリアルポートが開いた後用のコントロールを有効化
+            btnCommClose.Enabled = true;
+        }
+
+        private void Comm1Open()
+        {
+            serialPort1.BaudRate = 9600;
+            serialPort1.Parity = Parity.Even;
+            serialPort1.DataBits = 8;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.Handshake = Handshake.None;
+            serialPort1.PortName = "COM" + txtCommNumber.Text;
+            serialPort1.NewLine = "\r\n";
+
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Comm1Open Exception:" + ex.Message);
+            }
+            
+        }
+
+        private void btnCommClose_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+
+            // シリアルポートが開く前用のコントロールを有効化
+            btnCommOpen.Enabled = true;
+
+            // シリアルポートが開いた後用のコントロールを無効化
+            btnCommClose.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void OutCmd(string tcmd)
+        {
+            if (serialPort1.IsOpen == false)
+            {
+                return;
+            }
+
+            try
+            {
+                serialPort1.Write(tcmd + "\r\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("OutCmd Exception:" + ex.Message);
+            }
+            
+        }
+
+        private void btnSendCR_Click(object sender, EventArgs e)
+        {
+            OutCmd("CR");
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort1.ReadLine();
+            Response(data);
+        }
+
+        private void Response(string text)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(Response), text);
+                return;
+            }
+            lblReceivedText.Text += text + serialPort1.NewLine;
         }
     }
 }
